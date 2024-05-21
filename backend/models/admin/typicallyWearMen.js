88d0de5a0dbeefc70047abf356984255d053275e
CodeMentor <35169408+codementor726@users.noreply.github.com>
2024-05-20 20:13:01 -0500

    test

---
+++ backend/models/admin/typicallyWearMen.js
@@ -0,0 +1,78 @@
+/**
+ * @author Sukhendu Mukherjee
+ * @email smukherjee@drapefit.com
+ * @developer li.yang@drapefit.com
+ * @developer roman.tyshanov@drapefit.com
+ * @coauthor suprakash.dev@drapefit.com
+ * @company Drape Fit Inc.
+ */
+import { sequelize } from '../../config/db.js';
+import { DataTypes } from 'sequelize';
+import Payment from './payment.js';
+import User from './user.js';
+
+const TypicallyWearMen = sequelize.define(
+  'typically_wear_men',
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    },
+    payment_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Payment,
+        key: 'id'
+      }
+    },
+    shirt: {
+      type: DataTypes.STRING
+    },
+    size: {
+      type: DataTypes.STRING
+    },
+    waist: {
+      type: DataTypes.STRING
+    },
+    waist_size_run: {
+      type: DataTypes.STRING
+    },
+    inseam: {
+      type: DataTypes.STRING
+    },
+    men_bottom: {
+      type: DataTypes.STRING
+    },
+    bleazer: {
+      type: DataTypes.STRING
+    },
+    shoe: {
+      type: DataTypes.STRING
+    },
+    shoe_medium: {
+      type: DataTypes.STRING
+    },
+    body_type: {
+      type: DataTypes.INTEGER,
+      defaultValue: -1
+    },
+    skin_tone: {
+      type: DataTypes.INTEGER,
+      defaultValue: -1
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'typically_wear_men'
+  }
+);
+TypicallyWearMen.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+TypicallyWearMen.belongsTo(Payment, {
+  foreignKey: 'payment_id'
+});
+export default TypicallyWearMen;
---
+++ frontend/src/views/client/profile/AddCard.js
@@ -0,0 +1,47 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+import MyEnvConfig from 'configs/MyEnvConfig';
+import CheckoutForm from '../component/profile/PayCheckoutForm';
+import 'assets/scss/_stripe.scss';
+import { createCustomerSecretMethod } from 'actions/payment';
+import { useEffect, useState } from 'react';
+
+const stripePromise = loadStripe(`${MyEnvConfig.stripe.pbKey}`);
+
+const AddCard = () => {
+  const appearance = {
+    theme: 'stripe'
+  };
+
+  const [options, setOptions] = useState({
+    // passing the client secret obtained from the server
+    appearance
+  });
+
+  const [userId, setUserId] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const body = await createCustomerSecretMethod();
+      const options = {
+        clientSecret: body.clientSecret,
+        appearance
+      };
+      setOptions(options);
+      setUserId(body?.user?.id);
+    })();
+  }, []);
+
+  return (
+    <div className="stripe">
+      {options.clientSecret && (
+        <Elements options={options} stripe={stripePromise} style={{ width: '100%' }}>
+          <CheckoutForm userId={userId} />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default AddCard;
